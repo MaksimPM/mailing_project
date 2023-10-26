5c6ddf43c3b72f7fa86ed4f59985a6d5929a9f12
Maksim Pakhomov <maksim-paxomov-2000@mail.ru>
2023-10-26 19:36:39 +0300

    last commit

--- users/management/commands/cgm.py
+++ users/management/commands/cgm.py
@@ -8,9 +8,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         group, created = Group.objects.get_or_create(name='manager')
         if created:
-            permissions = Permission.objects.filter(
-                content_type__model__in=['newsletter', 'message', 'client', 'user']).filter(
-                name__icontains='view') | Permission.objects.filter(codename__in=['set_activity', 'change_activity'])
+            permissions = Permission.objects.filter(content_type__model__in=['mailing', 'message', 'client', 'user'])\
+                .filter(name__icontains='view') | Permission.objects.filter(codename__in=['set_activity',
+                                                                                          'change_activity'])
 
             for perm in permissions:
                 group.permissions.add(perm)
